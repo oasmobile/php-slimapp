fcc2e5c254ea38689155c1a97e3d44c45416c53a
Minhao Zhang <minhao.zhang@qq.com>
2016-09-28 17:56:39 +0800

    * deprecate abstrace command for daemon sentinel

--- src/SentinelCommand/AbstractDaemonSentinelCommand.php
+++ src/SentinelCommand/AbstractDaemonSentinelCommand.php
@@ -15,6 +15,12 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
 
+/**
+ * Class AbstractDaemonSentinelCommand
+ *
+ * @deprecated
+ * @package Oasis\SlimApp\SentinelCommand
+ */
 class AbstractDaemonSentinelCommand extends AbstractAlertableCommand
 {
     protected $runningProcesses = [];
---
+++ src/SentinelCommand/DaemonSentinelCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minhao
+ * Date: 2016-09-28
+ * Time: 17:55
+ */
+
+namespace Oasis\SlimApp\SentinelCommand;
+
+/** @noinspection PhpDeprecationInspection */
+class DaemonSentinelCommand extends AbstractDaemonSentinelCommand
+{
+    
+}
