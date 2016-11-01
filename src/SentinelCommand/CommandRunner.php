d3c44dea81fec267eb7652bf2fb6e79a28d01cfa
Minhao Zhang <minhao.zhang@qq.com>
2016-11-01 14:51:44 +0800

    * add trace on/off switch to command runner for sentinel command

--- src/SentinelCommand/CommandRunner.php
+++ src/SentinelCommand/CommandRunner.php
@@ -17,23 +17,27 @@ class CommandRunner
 {
     /** @var  Application */
     protected $application;
-
+    
     protected $name;
     /** @var  InputInterface */
     protected $input;
     /** @var  OutputInterface */
     protected $output;
-
+    
     protected $once      = false;
     protected $interval  = 0;
     protected $frequency = 0;
-
-    protected $lastRun = 0;
-    protected $nextRun = 0;
-    protected $alert   = true;
-    protected $stopped = false;
-
-    public function __construct(Application $application, array $command, OutputInterface $output)
+    
+    protected $lastRun      = 0;
+    protected $nextRun      = 0;
+    protected $alert        = true;
+    protected $stopped      = false;
+    protected $traceEnabled = false;
+    
+    public function __construct(Application $application,
+                                array $command,
+                                OutputInterface $output,
+                                $traceEnabled = false)
     {
         $this->application = $application;
         $this->output      = $output;
@@ -41,64 +45,16 @@ class CommandRunner
         $args              = ['command' => $this->name];
         $args              = array_merge($args, $command['args']);
         $this->input       = new ArrayInput($args);
-
+        
         $this->once      = $command['once'];
         $this->interval  = $command['interval'];
         $this->frequency = $command['frequency'];
         $this->alert     = $command['alert'];
-
-        $this->nextRun = time();
+        
+        $this->nextRun      = time();
+        $this->traceEnabled = $traceEnabled;
     }
-
-    public function run()
-    {
-        if ($this->stopped) {
-            return 0;
-        }
-
-        $pid = pcntl_fork();
-        if ($pid < 0) {
-            $errno = pcntl_get_last_error();
-            throw new \RuntimeException("Cannot fork process, error = " . pcntl_strerror($errno));
-        }
-        elseif ($pid == 0) {
-            // in child process
-            $now = time();
-            if ($now < $this->nextRun) {
-                mnotice("Will wait %d seconds for next run of %s", $this->nextRun - $now, $this->name);
-                sleep($this->nextRun - $now);
-            }
-
-            // run using application
-            $this->application->setAutoExit(false); // we will handle exit on our own
-            $this->application->setCatchExceptions(false); // we will catch on our own
-            try {
-                $ret = $this->application->run($this->input, $this->output);
-            } catch (\Exception $e) {
-                mtrace($e, "Exception while running command {$this->name}", "error");
-                $ret = AbstractDaemonSentinelCommand::EXIT_CODE_COMMON_ERROR;
-            }
-
-            // Check if we should alert
-            if ($ret != AbstractDaemonSentinelCommand::EXIT_CODE_OK
-                && $this->alert
-            ) {
-                // alert in child process is better, because we can get more trace here
-                malert("Daemon command %s failed with exit code = %d", $this->name, $ret);
-
-                exit(AbstractDaemonSentinelCommand::EXIT_CODE_OK); // exit OK because alert is already sent
-            }
-            else {
-                exit($ret);
-            }
-        }
-        else {
-            $this->lastRun = $this->nextRun;
-
-            return $pid;
-        }
-    }
-
+    
     public function onProcessExit($exitStatus, $pid)
     {
         if ($exitStatus != 0) {
@@ -109,7 +65,7 @@ class CommandRunner
                 mwarning("Daemon command %s failed with exit code = %d", $this->name, $exitStatus);
             }
         }
-
+        
         if ($this->once) {
             $this->stopped = true;
         }
@@ -124,14 +80,67 @@ class CommandRunner
                 }
             }
         }
-
-        mdebug(
-            "Process [%d] exits for command %s, exit code = %d, last run = %d, next run = %d",
-            $pid,
-            $this->name,
-            $exitStatus,
-            $this->lastRun,
-            $this->nextRun
-        );
+        
+        if ($this->traceEnabled) {
+            mdebug(
+                "Process [%d] exits for command %s, exit code = %d, last run = %d, next run = %d",
+                $pid,
+                $this->name,
+                $exitStatus,
+                $this->lastRun,
+                $this->nextRun
+            );
+        }
+    }
+    
+    public function run()
+    {
+        if ($this->stopped) {
+            return 0;
+        }
+        
+        $pid = pcntl_fork();
+        if ($pid < 0) {
+            $errno = pcntl_get_last_error();
+            throw new \RuntimeException("Cannot fork process, error = " . pcntl_strerror($errno));
+        }
+        elseif ($pid == 0) {
+            // in child process
+            $now = time();
+            if ($now < $this->nextRun) {
+                if ($this->traceEnabled) {
+                    mdebug("Will wait %d seconds for next run of %s", $this->nextRun - $now, $this->name);
+                }
+                sleep($this->nextRun - $now);
+            }
+            
+            // run using application
+            $this->application->setAutoExit(false); // we will handle exit on our own
+            $this->application->setCatchExceptions(false); // we will catch on our own
+            try {
+                $ret = $this->application->run($this->input, $this->output);
+            } catch (\Exception $e) {
+                mtrace($e, "Exception while running command {$this->name}", "error");
+                $ret = AbstractDaemonSentinelCommand::EXIT_CODE_COMMON_ERROR;
+            }
+            
+            // Check if we should alert
+            if ($ret != AbstractDaemonSentinelCommand::EXIT_CODE_OK
+                && $this->alert
+            ) {
+                // alert in child process is better, because we can get more trace here
+                malert("Daemon command %s failed with exit code = %d", $this->name, $ret);
+                
+                exit(AbstractDaemonSentinelCommand::EXIT_CODE_OK); // exit OK because alert is already sent
+            }
+            else {
+                exit($ret);
+            }
+        }
+        else {
+            $this->lastRun = $this->nextRun;
+            
+            return $pid;
+        }
     }
 }
