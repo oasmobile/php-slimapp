c5e0cfb1d0427af369e8556f7e2d164d2e6e553b
Minhao Zhang <minhao.zhang@qq.com>
2026-05-02 23:48:29 +0800

    *(test) by Kiro: 抑制测试中的日志脏输出
    在 bootstrap 中安装 NullHandler 并设 minLogLevel 为 EMERGENCY+1，
    阻止 MLogging::log() 自动安装 ConsoleHandler 导致的 stderr 输出

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,9 @@ $previousHandler = set_error_handler(function ($errno, $errstr) use (&$previousH
 });
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+// 抑制测试过程中 oasis/logging 的全局日志输出（mtrace/malert/mwarning/mdebug 等）
+// 必须先安装一个 handler（否则 MLogging::log 会自动安装 ConsoleHandler），再设最高级别
+$nullHandler = new Monolog\Handler\NullHandler(Monolog\Logger::DEBUG);
+Oasis\Mlib\Logging\MLogging::addHandler($nullHandler, 'test_null');
+Oasis\Mlib\Logging\MLogging::setMinLogLevel(Monolog\Logger::EMERGENCY + 1);
